161d5c399bdae677cfeecd1b7a06b09335600b43
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2021-09-11 20:15:37 -0400

    Initial tested commit

---
+++ 3.4.0/modulefile.lua
@@ -0,0 +1,35 @@
+--
+-- AUGUSTUS 3.4.0 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: AUGUSTUS is a program that predicts genes in eukaryotic genomic sequences."
+-- "Keywords: singularity bioinformatics genomics"
+
+whatis("Name: AUGUSTUS")
+whatis("Version: 3.4.0")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Keywords: singularity bioinformatics genomics")
+whatis("Description: AUGUSTUS is a program that predicts genes in eukaryotic genomic sequences.")
+
+help([[
+AUGUSTUS is a program that predicts genes in eukaryotic genomic sequences.
+
+To load the module type
+
+> module load AUGUSTUS/3.4.0
+
+To unload the module type
+
+> module unload AUGUSTUS/3.4.0
+
+Tools included in this module are
+
+* augustus
+]])
+
+local package = "AUGUSTUS"
+local version = "3.4.0"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
